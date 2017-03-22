aae03541b54633eaff9bd75027fb710780d80a87
belhyun <belhyun@coupang.com>
2017-03-22 23:49:12 +0900

    [YM-20170321] 메일 전송 서비스 개발중

--- src/main/java/com/yonsei/YonseimarketApplication.java
+++ src/main/java/com/yonsei/YonseimarketApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YonseimarketApplication {
+public class YonseiMarketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YonseimarketApplication.class, args);
+		SpringApplication.run(YonseiMarketApplication.class, args);
 	}
 }
